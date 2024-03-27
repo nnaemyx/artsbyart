060634cdf715d9270b7e5fbbb206347bd2ca3794
nnaemyx <edehjohnpaul@gmail.com>
2024-03-27 12:43:16 -0400

    new

--- src/pages/api/sms.js
+++ src/pages/api/sms.js
@@ -3,9 +3,9 @@ import twilio from "twilio";
 
 export default  async function POST(request) {
   // Twilio credentials and configuration
-  const accountSid = "ACd1a4fd485e2a5e65b632ad4243eea338";
-  const authToken = "a4eb5ec6092a51de89b2a5a942ebf088";
-  const from = "+19282362066";
+  const accountSid = process.env.YOUR_TWILIO_ACCOUNT_SID;
+  const authToken = process.env.YOUR_TWILIO_AUTH_TOKEN;
+  const from = process.env.YOUR_TWILIO_PHONE_NUMBER;
   const client = require("twilio")(accountSid, authToken);
 
   // Extract phone number from the request body
