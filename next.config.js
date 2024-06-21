ad91ee5a03947a91fa5ccaeffce1d72575be0af6
cyberjay <edehjohnpaul@gmail.com>
2024-06-21 17:24:13 +0100

    new

--- next.config.js
+++ next.config.js
@@ -5,15 +5,6 @@ const nextConfig = {
     domains: ['res.cloudinary.com'],
     // hostname: ["res.cloudinary.com"],
   },
-  redirects: async () => {
-    return [
-      {
-        has: [{ type: 'header', key: 'host', value: 'www.artsbyart.com' }],
-        destination: 'https://www.artsbyart.com',
-        permanent: true,
-      },
-    ];
-  },
   experimental: {
     runtime: 'edge',
   },
